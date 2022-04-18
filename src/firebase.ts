fa73b9b893584213f793a0f15b565e004cf93305
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2022-04-18 11:01:17 -0300

    Setup App Check

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
 	apiKey: 'AIzaSyBIao2LuScDC-HarbmWksgdVS9-TDPmcAs',
 	authDomain: 'inquietos-3662b.firebaseapp.com',
@@ -12,8 +11,6 @@ const firebaseConfig = {
 	appId: '1:341738130413:web:2e5c94e0edbdf1167235e4'
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
-// Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
